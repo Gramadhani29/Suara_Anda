4d3622e1cdda24f45c64441a044322265ce5bbce
Gilang Ramadhani Al <gilang.alifianto@gmail.com>
2024-12-30 23:35:24 +0700

    login

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
